9aaaab9ec215b87876b1ed09721ab5b706de1a74
BuildTools <unconfigured@null.spigotmc.org>
2020-03-21 15:55:14 -0500

    clean up

--- src/main/java/me/yodeling_goat/hoox/Commands/SaveCommand.java
+++ src/main/java/me/yodeling_goat/hoox/Commands/SaveCommand.java
@@ -10,40 +10,27 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 
 public class SaveCommand implements CommandExecutor {
+	Hoox plugin;
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-
 		if (sender instanceof Player) {
 			Player player = (Player) sender;
 			if (player.hasPermission("Hoox.SaveCommand")) {
-				// get the URL from the config
-				// call save with the url
-				String url = "get the url";
+				String url = plugin.getConfig().getString("url_save");
 				save(player, url);
-				player.sendMessage(ChatColor.BLUE + "You have the permission to use this command");
 			} else {
-				player.sendMessage(ChatColor.RED + "You are not authorized to use this command you must have the permissions in order to do so.");
+				player.sendMessage(ChatColor.RED + "You are not authorized to save.");
 			}
 		} else {
 			System.out.println("You are not a player you are not authorized to use this command!!!");
 		}
-
-
 		return true;
 	}
-
-	Hoox plugin;
-
 	public SaveCommand(Hoox plugin){
 		this.plugin = plugin;
 	}
-
-
 	private void save(Player player, String url) {
-	player.sendMessage(ChatColor.GRAY + "Something happened");
-	for (int i = 0; i < plugin.getConfig().getList("message").size(); i++) {
-		player.sendMessage(plugin.getConfig().getList("message").get(i).toString());
-	}
+		player.sendMessage(ChatColor.GRAY + "Something happened to " + url);
 	}
 }
